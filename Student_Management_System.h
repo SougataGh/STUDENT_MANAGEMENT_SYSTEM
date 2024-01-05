4dbaa32462088ee0522f55d2d0bc4dc0e6050e1c
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-05 23:44:33 +0530

    Create Student_Management_System.h

---
+++ Student_Management_System.h
@@ -0,0 +1,9 @@
+class Student_Management_System{
+	Regular_Student st1;
+	stream_change_Student st2;
+	management_Student st3;
+	public:
+		void get_Regular_Students();
+		void get_stream_change_Students();
+		void get_management_Students();
+};
