482f7d2a6a46d4dbc1067e8f43c864ff25395690
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-05 23:47:40 +0530

    Update stream_change_Student.h

--- stream_change_Student.h
+++ stream_change_Student.h
@@ -1,11 +1,8 @@
 class stream_change_Student : public Regular_Student {
     string new_stream;
 
-protected:
+public:
     void add_student();
     void show_all_student();
     void search_student();
-
-public:
-    virtual void get_Student();
 };
