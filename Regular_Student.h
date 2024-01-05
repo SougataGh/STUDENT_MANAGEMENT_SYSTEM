9ade568af67a7e00127bca71ebb0e6dd19451ed2
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-05 22:53:50 +0530

    Update Regular_Student.h

--- Regular_Student.h
+++ Regular_Student.h
@@ -6,12 +6,6 @@ using namespace std;
 class Regular_Student {
 protected:
     string name, roll_no, id, dob;
-    Student() {
-        name = "NO_NAME";
-        roll_no = "-1";
-        id = "-1";
-        dob = "-1";
-    }
    virtual void add_student();
    virtual void show_all_student();
    virtual void search_student();
