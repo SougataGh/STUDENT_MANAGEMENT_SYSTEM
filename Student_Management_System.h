e54aad187fc27b5e3043f554c5816b0f864301ce
Sougata Ghosh <155520429+SougataGh@users.noreply.github.com>
2024-01-07 12:46:07 +0530

    Update Student_Management_System.h

--- Student_Management_System.h
+++ Student_Management_System.h
@@ -3,6 +3,7 @@ class Student_Management_System{
 	//stream_change_Student st2;
 	//management_Student st3;
 	public:
+                Regular_Student();
 		void get_Regular_Students();
 		void get_stream_change_Students();
 		void get_management_Students();
